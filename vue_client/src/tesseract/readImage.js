3a6c3514e44318c3f1da48267308305b36d36c09
jedmonson <jedmonson1478@gmail.com>
2021-08-14 18:21:23 -0400

    removing tesseract demos

--- vue_client/src/tesseract/readImage.js
+++
@@ -1,9 +0,0 @@
-const Tesseract =  require('tesseract.js');
-
-Tesseract.recognize(
-  'image.jpg',
-  'eng',
-  { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-  console.log(text);
-})
